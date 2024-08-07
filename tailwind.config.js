857d7c69602b28192cec5f8aeec4d649be7c1903
Mykhailo <mishamyrosh03@gmail.com>
2024-08-07 15:00:31 +0300

    Chanegs dependices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
